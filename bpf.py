804c38ca6ce16dc47519104f22ad09c173f581bb
seeul8er <mail@wolfgangchristl.de>
2017-09-15 21:42:49 +0200

    bugfix BPF filter

--- bpf.py
+++ bpf.py
@@ -25,7 +25,7 @@ def attach_filter(sock, direction, dst_mac, port):
     # first byte of dst_mac must be 0x01, we overwrite it here to be sure
     version_port = b'\x01'+port
     dest_mac2 = int.from_bytes(dst_mac[2:6], byteorder='big', signed=False)
-    dest_mac1 = int.from_bytes(bytearray(b'\x01'+dst_mac[1:2]), byteorder='big', signed=False)
+    dest_mac1 = int.from_bytes(bytearray(b'\x01' + direction), byteorder='big', signed=False)
     print(dst_mac)
     BPFFILTER = [
         [0x30, 0, 0, 0x00000003],
@@ -39,9 +39,9 @@ def attach_filter(sock, direction, dst_mac, port):
         [0x45, 1, 0, 0x00000008], # allow data frames
         [0x45, 0, 9, 0x00000080], # allow beacon frames
         [0x40, 0, 0, 0x00000006],
-        [0x15, 0, 7, dest_mac2], # dest_mac of packet should be mac of local interface
+        [0x15, 0, 7, dest_mac2], # <comm_id>
         [0x48, 0, 0, 0x00000004],
-        [0x15, 0, 5, dest_mac1], # dest_mac of packet should be mac of local interface
+        [0x15, 0, 5, dest_mac1], # <odd><direction>
         [0x48, 0, 0, 0x00000010],
         [0x15, 0, 3, int.from_bytes(version_port, byteorder='big', signed=False)], # e.g. version = 0x01 port = 0x02
         [0x50, 0, 0, 0x00000012],
